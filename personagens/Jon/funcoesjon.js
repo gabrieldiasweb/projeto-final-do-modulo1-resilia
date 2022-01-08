ea2c885e9a95d0d3fb03d1928cf4edb39163e061
gabrieldiasweb <82773946+gabrieldiasweb@users.noreply.github.com>
2022-01-08 15:19:18 -0300

    Criação de pagina e função
    Foi criada a Pagina jon2_2.html e adicionado uma nova Função no arquivo funcoesjon.js.

--- personagens/Jon/funcoesjon.js
+++ personagens/Jon/funcoesjon.js
@@ -15,3 +15,18 @@ function jon1_1() {
 	}
   }
 //SEGUNDA FUNÇÃO JON 
+function jon1_2() {
+	while (pergunta1 != 1 || pergunta1 != 2) {
+	  var pergunta1 = prompt("Digite 1 para Fica Treinando na Academia \n 2 Para Vai curtir a baladinha Marota");
+  
+	  if (pergunta1 == 1) {
+		window.location.href = "gameOverJon1.html";
+		break;
+	  } else if (pergunta1 == 2) {
+		window.location.href = "jon2_2.html";
+		break;
+	  } else {
+		alert("Digite 1 ou 2");
+	  }
+	}
+  }
